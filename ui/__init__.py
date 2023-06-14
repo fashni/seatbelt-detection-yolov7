1f2bf30ed21026e5b0a18595389f801b5b98c445
fashni <fashni@tuta.io>
2023-06-14 12:13:44 +0700

    remove unused files

--- ui/__init__.py
+++ ui/__init__.py
@@ -3,5 +3,3 @@ from .FrameWidget import FrameWidget
 from .DoubleSpinBox import DoubleSpinBox
 from .SideBar import SideBar
 from .MainWidget import MainWidget
-from .VideoThread import VideoThread
-
